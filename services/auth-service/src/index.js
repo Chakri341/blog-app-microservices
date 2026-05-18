81b0d7a4aa7a9351defeb3c1be3a938b1142f273
Chakradhar <potluruchakradhar7@gmail.com>
2026-05-18 18:53:36 +0530

    added auth-service

---
+++ services/auth-service/src/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+
+import cors from "cors";
+
+import dotenv from "dotenv";
+
+import authRoutes from
+"./routes/auth.routes.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(
+  "/api/auth",
+  authRoutes
+);
+
+app.get("/", (req, res) => {
+
+  res.send(
+    "Auth Service Running"
+  );
+
+});
+
+const PORT =
+  process.env.PORT || 8001;
+
+app.listen(PORT, () => {
+
+  console.log(
+    `Auth Service running on ${PORT}`
+  );
+
+});
---
+++ services/auth-service/src/routes/auth.routes.js
@@ -0,0 +1,30 @@
+import express from "express";
+
+import {
+    register,
+    login,
+    profile,
+} from "../controllers/auth.controller.js";
+
+import authMiddleware from
+    "../middlewares/auth.middleware.js";
+
+const router = express.Router();
+
+router.post(
+    "/register",
+    register
+);
+
+router.post(
+    "/login",
+    login
+);
+
+router.get(
+    "/profile",
+    authMiddleware,
+    profile
+);
+
+export default router;
